f6ee2efc623ad440c74dcd3df261df25b3174dca
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-15 17:41:20 -0300

    Requesito 4.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -35,6 +36,9 @@ class Header extends React.Component {
       <header data-testid="header-component">
         { loading && <Loading /> }
         <span data-testid="header-user-name">{`Bem vindo ${name} `}</span>
+        <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </header>
     );
   }
