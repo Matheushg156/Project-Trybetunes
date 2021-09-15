a85ffc7adc1ff87df6d8ca181c60cb6a5ec396a0
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-15 18:01:40 -0300

    Requesito 5.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,46 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputSearch: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { inputSearch } = this.state;
+    const minLength = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            name="inputSearch"
+            value={ inputSearch }
+            onChange={ this.handleChange }
+            type="text"
+            placeholder="Nome do Artista"
+          />
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ inputSearch.length < minLength }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
