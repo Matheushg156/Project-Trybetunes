81ea9b600c3e352f587141fe59a099ba8ef86663
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-16 19:08:27 -0300

    Stiliza a pagina de login.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import { createUser } from '../services/userAPI';
 import Loading from '../components/Loading';
+import logo from '../images/logotrybe.png';
 
 class Login extends React.Component {
   constructor() {
@@ -26,10 +27,13 @@ class Login extends React.Component {
 
   async fetchCreatUser() {
     const { user } = this.state;
+
     this.setState({ loading: true });
+
     await createUser({
       name: user,
     });
+
     this.setState({
       loading: false,
       logged: true,
@@ -40,27 +44,32 @@ class Login extends React.Component {
     const { user, loading, logged } = this.state;
     const minLength = 3;
     return (
-      <div data-testid="page-login">
-        { logged && <Redirect to="/search" /> }
-        <form>
-          <input
-            name="user"
-            type="text"
-            data-testid="login-name-input"
-            placeholder="Nome"
-            value={ user }
-            onChange={ this.handleChange }
-          />
-          <button
-            type="button"
-            disabled={ user.length < minLength }
-            data-testid="login-submit-button"
-            onClick={ this.fetchCreatUser }
-          >
-            Entrar
-          </button>
-          {loading && <Loading /> }
-        </form>
+      <div className="mainConteiner">
+        <img src={ logo } alt="logo trybetunes" className="logoTrybetubes" />
+        <div data-testid="page-login" className="containerLogin">
+          { logged && <Redirect to="/search" /> }
+          <form className="loginStyle">
+            <input
+              className="inputLogin"
+              name="user"
+              type="text"
+              data-testid="login-name-input"
+              placeholder="Nome"
+              value={ user }
+              onChange={ this.handleChange }
+            />
+            <button
+              className="buttonLogin"
+              type="button"
+              disabled={ user.length < minLength }
+              data-testid="login-submit-button"
+              onClick={ this.fetchCreatUser }
+            >
+              Entrar
+            </button>
+            {loading && <Loading /> }
+          </form>
+        </div>
       </div>
     );
   }
