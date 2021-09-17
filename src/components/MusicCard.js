7beff7d73468e1578e4f24d9f553e2599fa4e513
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-17 13:51:33 -0300

    Começa o requesito 8.

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      favorite: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async handleClick(event, music) {
+    if (event.target.checked) {
+      await addSong(music);
+      return this.setState({
+        favorite: true,
+      });
+    }
+    await removeSong(music);
+    return this.setState({
+      favorite: false,
+    });
+  }
+
   render() {
     const { arrayOfMusics } = this.props;
+    const { favorite } = this.state;
     return (
       <div>
-        { arrayOfMusics.slice(1).map((music, index) => (
-          <section key={ index }>
+        { arrayOfMusics.slice(1).map((music) => (
+          <section key={ music.trackId }>
             <div>{music.trackName}</div>
 
             <audio data-testid="audio-component" src={ music.previewUrl } controls>
@@ -16,6 +41,17 @@ class MusicCard extends React.Component {
               <code>audio</code>
               .
             </audio>
+            <label
+              htmlFor={ music.trackId }
+              data-testid={ `checkbox-music-${music.trackId}` }
+            >
+              <input
+                id={ music.trackId }
+                type="checkbox"
+                checked={ favorite }
+                onChange={ this.handleClick }
+              />
+            </label>
           </section>
         ))}
       </div>
